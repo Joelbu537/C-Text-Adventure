44c2d95ddb8c2aa0a5b94e1f1959e0f038863f55
Jork Elias <jork-elias@outlook.de>
2026-01-22 11:13:32 +0100

    Improved picking up items

--- C# Text Adventure/ItemDefinitions.cs
+++ C# Text Adventure/ItemDefinitions.cs
@@ -7,8 +7,8 @@ public static class ItemDefinitions
         "A large, ripe, yellow, cob of corn. Best served with butter, but definetly edible in its current form.",
         0.5,
         5,
-        15
-        );
+        10
+    );
     public static Item HealingPotion = new HealingItem(
         "Healing Potion",
         "A clear glass bottle selaed with a cork. It contains a red liquid that smells like cheryy.",
@@ -17,6 +17,25 @@ public static class ItemDefinitions
         75
     );
 
-    public static Item TavernNote = new InfoItem("Note", "A piece of paper with something written on it", 0.01, 1,
-        "We need your help!\nMeet me in the shed on the " + Color.FORE_WHITE + "east" + Color.RESET + " side of the tavern!");
+    public static Item FabricLump = new HealingItem(
+        "Lump of fabric",
+        "A bunch of small pieces of fabric in bad condition. Can still be used to stop a bleeding or cover up a wound.",
+        0.5,
+        5,
+        25
+    );
+    public static WeaponItem Glock19 = new WeaponItem(
+        "Glock 19",
+        "Its a Glock 19. From the future. Infinite ammo included.",
+        0.8,
+        750,
+        100
+    );
+
+    public static Item TavernNote = new InfoItem(
+        "Note", "A piece of paper with something written on it",
+        0.01,
+        1,
+        "We need your help!\nMeet me in the shed on the " + Color.FORE_WHITE + "east" + Color.RESET + " side of the tavern!"
+    );
 }
